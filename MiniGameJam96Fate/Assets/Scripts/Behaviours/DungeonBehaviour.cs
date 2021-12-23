eb887a5fe185fa0fca309d6ba1e03a2a53d08399
Kevin <kasmeltz@lakeheadu.ca>
2021-12-23 14:31:08 -0500

    try that shit

---
+++ MiniGameJam96Fate/Assets/Scripts/Behaviours/DungeonBehaviour.cs
@@ -0,0 +1,54 @@
+﻿namespace HairyNerdStudios.GameJams.MiniGameJam96.Unity.Behaviours
+{
+    using HairyNerdStudios.GameJams.MiniGameJam96.Unity.Logic;
+    using UnityEngine;
+    using UnityEngine.Tilemaps;
+
+    [AddComponentMenu("HairyNerd/MGJ96/Dungeon")]
+    public class DungeonBehaviour : BehaviourBase
+    {
+        #region Members
+
+        public Tilemap Walls;
+
+        public TileBase[] WallTiles;
+
+        #endregion
+
+        #region Unity
+
+        public void Awake()
+        {
+            var dungeon = new Dungeon
+            {
+                Width = 40,
+                Height = 23,
+                RoomThreshold = 0.5,
+                RoomOverlapAmount = -1,
+                MinRoomWidth = 1,
+                MaxRoomWidth = 10,
+                MinRoomHeight = 1,
+                MaxRoomHeight = 10
+            };
+
+            dungeon
+                .Build();
+
+            for (int y = 0; y < dungeon.Height; y++)
+            {
+                for (int x = 0; x < dungeon.Width; x++)
+                {
+                    int wall = dungeon.Walls[y, x];
+
+                    if (wall == 1)
+                    {
+                        Walls
+                            .SetTile(new Vector3Int(x - dungeon.Width / 2, y - dungeon.Height / 2, 0), WallTiles[0]);
+                    }
+                }
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ MiniGameJam96Fate/Assets/Scripts/Logic/Room.cs
@@ -0,0 +1,49 @@
+﻿namespace HairyNerdStudios.GameJams.MiniGameJam96.Unity.Logic
+{
+    using UnityEngine;
+
+    public class Room
+    {
+        public Room(int x, int y, int w, int h)
+        {
+            X = x;
+            Y = y;
+            Width = w;
+            Height = h;
+
+            TopLeft = new Vector2(x, y);
+            BottomRight = new Vector2(x + w, y + h);
+        }
+
+        public int X { get; set; }
+
+        public int Y { get; set; }
+
+        public int Width { get; set; }
+
+        public int Height { get; set; }
+
+        public Vector2 TopLeft { get; set; }
+
+        public Vector2 BottomRight { get; set; }
+
+        public bool DoesOverlap(Room other, int? padding = null)
+        {
+            if (!padding.HasValue)
+            {
+                padding = 0;
+            }
+
+            if (TopLeft.x >= other.BottomRight.x + padding ||
+                other.TopLeft.x >= BottomRight.x + padding ||
+                BottomRight.y < other.TopLeft.y - padding ||
+                other.BottomRight.y < TopLeft.y - padding)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+
+}
