eb678548265c57a3c65a60375038b4af521f1286
Ivan <75530916+RaveZ@users.noreply.github.com>
2021-12-25 12:19:16 +0800

    -

--- MiniGameJam96Fate/Assets/Scripts/Behaviours/RadarBehaviour.cs
+++ MiniGameJam96Fate/Assets/Scripts/Behaviours/RadarBehaviour.cs
@@ -1,23 +1,37 @@
-using UnityEngine;
-
-public class RadarBehaviour : MonoBehaviour
+namespace HairyNerdStudios.GameJams.MiniGameJam96.Unity.Behaviours
 {
-    #region Members
+    using UnityEngine;
 
-    
-
-    [SerializeField] private Transform PlayerPivot = null;
-
-    #endregion
-
-    void Start()
+    public class RadarBehaviour : ActorBehaviour
     {
-        
-    }
+        #region Members
 
-    // Update is called once per frame
-    void FixedUpdate()
-    {
-        transform.RotateAround(PlayerPivot.localPosition, Vector3.back, Time.deltaTime * 10f);
+        protected ReaperBehaviour Reaper { get; set; }
+
+        [SerializeField] private Transform PlayerPivot = null;
+
+        #endregion
+
+        protected override void Awake()
+        {
+            base
+                .Awake();
+
+            Reaper = FindObjectOfType<ReaperBehaviour>();
+        }
+
+        // Update is called once per frame
+        void FixedUpdate()
+        {
+            if (Reaper.gameObject.active)
+            {
+                gameObject.SetActive(true);
+                transform.RotateAround(PlayerPivot.localPosition, Vector3.back, Time.deltaTime * 10f);
+            }
+            else
+            {
+                gameObject.SetActive(false);
+            }
+        }
     }
 }
